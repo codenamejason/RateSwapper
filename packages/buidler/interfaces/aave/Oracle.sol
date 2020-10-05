b5e408899af0a922efe3e46ff71e34c41db3554f
codenamejason <codenamejason@outlook.com>
2020-10-05 08:22:26 -0400

    starting over

--- packages/buidler/interfaces/aave/Oracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.7.0;
-
-interface Oracle {
-    function getAssetPrice(address reserve) external view returns (uint256);
-
-    function latestAnswer() external view returns (uint256);
-}
--- packages/buidler/interfaces/compound/Comptroller.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.6.0 <0.7.0;
-
-interface Comptroller {
-    function claimComp(address holder) external;
-}
